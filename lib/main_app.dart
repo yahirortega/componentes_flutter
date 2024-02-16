c06ea72636a1041671484dd7a84452ff12ad5e2e
yahir <gemah830@gmail.com>
2024-02-16 10:34:07 -0600

    Add  a Simple ListViw

---
+++ lib/main_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:componentes/home_screen.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
